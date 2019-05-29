753e79447f90ab1f455636dbf7026c2a4c987bb6
Dian <cbtis16.pelayodian.h@gmail.com>
2019-05-29 14:50:24 -0500

    2
    segunda subida

--- js/main.js
+++ js/main.js
@@ -2,35 +2,35 @@ var audio;
 
 //Hide Pause Initially
 $('#pause').hide();
-
-//initializer- Play First Song
-initAudio($('#playlist li:first-child'));
-
-function initAudio(element){
-	var song=element.attr('song');
-	var title=element.text();
-	var cover=element.attr('cover');
-	var artist=element.attr('artist');
 	
+//Initializer - Play First Song
+initAudio($('#playlist li:first-child'));
+	
+function initAudio(element){
+	var song = element.attr('song');
+    var title = element.text();
+    var cover = element.attr('cover');
+    var artist = element.attr('artist');
+
 	//Create a New Audio Object
-	audio=new Audio('media/' + song);
+	audio = new Audio('media/' + song);
 	
 	if(!audio.currentTime){
 		$('#duration').html('0.00');
 	}
-	
+
 	$('#audio-player .title').text(title);
-	$('#audio-player .artist').text(artist);
+    $('#audio-player .artist').text(artist);
 	
 	//Insert Cover Image
 	$('img.cover').attr('src','images/covers/' + cover);
 	
 	$('#playlist li').removeClass('active');
-	element.addClass('active');
+    element.addClass('active');
 }
 
 
-//Play Button 
+//Play Button
 $('#play').click(function(){
 	audio.play();
 	$('#play').hide();
@@ -40,25 +40,49 @@ $('#play').click(function(){
 });
 
 //Pause Button
-$('#pause').click (function(){
+$('#pause').click(function(){
 	audio.pause();
 	$('#pause').hide();
 	$('#play').show();
 });
-
-//Stop Button 
+	
+//Stop Button
 $('#stop').click(function(){
-	audio.pause();
-	audio.currentTime=0;
+	audio.pause();		
+	audio.currentTime = 0;
 	$('#pause').hide();
 	$('#play').show();
 	$('#duration').fadeOut(400);
 });
 
-//Playist Song Click
-$('#playlist li').click(function(){
-	audio.pause();
-	initAudio($(this));
+//Next Button
+$('#next').click(function(){
+    audio.pause();
+    var next = $('#playlist li.active').next();
+    if (next.length == 0) {
+        next = $('#playlist li:first-child');
+    }
+    initAudio(next);
+	audio.play();
+	showDuration();
+});
+
+//Prev Button
+$('#prev').click(function(){
+    audio.pause();
+    var prev = $('#playlist li.active').prev();
+    if (prev.length == 0) {
+        prev = $('#playlist li:last-child');
+    }
+    initAudio(prev);
+	audio.play();
+	showDuration();
+});
+
+//Playlist Song Click
+$('#playlist li').click(function () {
+    audio.pause();
+    initAudio($(this));
 	$('#play').hide();
 	$('#pause').show();
 	$('#duration').fadeIn(400);
@@ -66,37 +90,26 @@ $('#playlist li').click(function(){
 	showDuration();
 });
 
-//volume Control
+//Volume Control
 $('#volume').change(function(){
-	audio.volume=parseFloat(this.value/10);
+	audio.volume = parseFloat(this.value / 10);
 });
-
+	
 //Time Duration
 function showDuration(){
-	$(audio).bind('timeupdate',function(){
+	$(audio).bind('timeupdate', function(){
 		//Get hours and minutes
-		var s=parseInt(audio.currentTime%60);
-		var m=parseInt((audio.currentTime/60)%60);
+		var s = parseInt(audio.currentTime % 60);
+		var m = parseInt((audio.currentTime / 60) % 60);
 		//Add 0 if seconds less than 10
-		if(s < 10){
-			s ='0' + s;
-	}
-	$('#duration').html(m + '.' + s);
-	var value=0;
-	if(audio.currentTime>0){
-		value=Math.floor((100/audio.duration)* audio.currentTime);
-	}
-	$('#progress').css('width',value +'%');
-});
+		if (s < 10) {
+			s = '0' + s;
+		}
+		$('#duration').html(m + '.' + s);	
+		var value = 0;
+		if (audio.currentTime > 0) {
+			value = Math.floor((100 / audio.duration) * audio.currentTime);
+		}
+		$('#progress').css('width',value+'%');
+	});
 }
-
-	
-	
-	
-	
-	
-	
-	
-	
-
-//Hide Pause Initially
